c580f8f89e426e8f53b720869e87789dc9f3c7d7
Renys De La Cruz <ronal2w@gmail.com>
2020-12-05 15:48:40 -0400

    Map location added to modalViewMore

--- src/routes/home/modalViewMore.tsx
+++ src/routes/home/modalViewMore.tsx
@@ -1,6 +1,8 @@
-import { Avatar, Button, Card, CardContent, CardHeader, CardMedia, CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Paper, Typography } from "@material-ui/core";
-import { CheckRounded, VerifiedUser, VerifiedUserOutlined } from "@material-ui/icons";
 import React, { useState } from "react";
+import { Avatar, Button, Card, CardContent, CardHeader, CardMedia, CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Icon, Paper, Typography } from "@material-ui/core";
+import { CheckRounded, VerifiedUser, VerifiedUserOutlined } from "@material-ui/icons";
+//@ts-ignore
+import GoogleMapReact from 'google-map-react';
 import { toast } from "react-toastify";
 //Custom components
 import PickImage from "../../components/pickImage";
@@ -11,6 +13,9 @@ import * as FirestoreService from "../../service/firestore";
 import { COLORS } from "../../utils/enums";
 import { getUIDCode } from "../../utils/functions";
 import { IFComment, IFPost } from "../../utils/interfaces";
+
+//@ts-ignore
+const AnyReactComponent = ({ lat, lng }) => <Icon style={{ color: COLORS.PRIMARY }} size={24} >room</Icon>;
 
 interface IModalPost {
     open: boolean,
@@ -58,8 +63,12 @@ function ModalPost(props: IModalPost) {
         } catch (error) {
             toast.error(error.message);
         }
-
     }
+
+    const goToLocation = () => {
+        window.open(`https://www.google.com/maps/search/${props.post.region.latitude},+${props.post.region.longitude}/@${props.post.region.latitude},${props.post.region.longitude},17z`)
+    }
+    
 
     return (
         <Dialog open={props.open} onClose={props.onClose} fullWidth maxWidth="sm" >
@@ -75,6 +84,21 @@ function ModalPost(props: IModalPost) {
                     </Grid>
                     <DialogContentText>
                         {props.post.description}
+                        <br />
+                        <div style={{ height: 400, width: "100%" }}>
+                            <GoogleMapReact
+                                bootstrapURLKeys={{ key: "AIzaSyAyHYyijB8VhsVv0LnIxMxFvbYn5u1Koo4" }}
+                                // defaultCenter={this.props.center}
+                                defaultCenter={{ lat: props.post.region.latitude, lng: props.post.region.longitude }}
+                                defaultZoom={15}
+                            >
+                                <AnyReactComponent
+                                    lat={props.post.region.latitude}
+                                    lng={props.post.region.longitude}
+                                />
+                            </GoogleMapReact>
+                        </div>
+                        <Button onClick={goToLocation} >Ir a la ubicación</Button>
                     </DialogContentText>
                     {(props.post.comments as IFComment[]).map((comment: IFComment, key: number) => {
                         return (
